481b4fd1567ab3a6b1c39066d1b77b9866fcb7e9
Qasim <qasim.wardak2020@gmail.com>
2021-01-26 17:35:29 -0500

    user2 changes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Qasim");
         System.out.println("Changes between Staged and Local");
-        
+        I have made some changes
     }
 }
