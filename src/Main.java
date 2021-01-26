44b9e54bd3574927aace15ddccfa9a8c15801fa9
Qasim <qasim.wardak2020@gmail.com>
2021-01-26 17:30:32 -0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Qasim");
-        System.out.println("Changes between Staged and Local");
-        System.out.println("I am making some changes in my code");
+        System.out.println("I am making changes in my code");
     }
 }
