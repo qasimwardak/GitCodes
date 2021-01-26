2d3006e3ebfc69023904c0604f6e2dec1c91fea3
Qasim <qasim.wardak2020@gmail.com>
2021-01-26 17:16:19 -0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Qasim");
-        System.out.println("Second Commit");
+        System.out.println("Changes between Staged and Local");
+        System.out.println("I am making some changes in my code");
     }
 }
